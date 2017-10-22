f9be4fe61def8ce91ef16a8c5d8012ce33901186
Frank Moreno <frankmoreno1993@gmail.com>
2017-10-22 01:35:10 -0500

    node sub support request, but unstable

--- nats.js
+++ nats.js
@@ -17,20 +17,23 @@ module.exports = function(RED) {
 
     var node = this;
 
-    var sid = this.nc.subscribe(this.subject,  function(message, reply, subject) {
+    var sid = node.nc.subscribe(this.subject,  function(message, replyTo, subject) {
       //console.log('subject: ' + subject + 'message: ' + message);
-      var msg = {payload: ''};
-      msg.payload = {
-        'subject': subject,
-        'message': message
+      var msg = {
+        payload: message,
+        topic: subject
+      };
+
+      if(replyTo){
+        msg.replyTo = replyTo
       }
       node.send(msg);
     });
 
-    this.on('close', function() {
-      if (this.nc) {
-        this.nc.unsubscribe(this.subject,sid);
-        this.nc.close();
+    node.on('close', function() {
+      if (node.nc) {
+        node.nc.unsubscribe(this.subject,sid);
+        node.nc.close();
       }
     });
   }
@@ -50,8 +53,10 @@ module.exports = function(RED) {
     var node = this;
 
     this.on('input', function(msg) {
-      this.subject = msg.payload.subject || config.subject;
-      this.message = msg.payload.message || config.message;
+      //this.subject = msg.replyTo || msg.topic || config.subject;
+      console.log('pub: ',msg.replyTo)
+      this.subject = msg.replyTo || config.subject;
+      this.message = msg.payload || config.message;
       //console.log('subject: ' + this.subject + ' message: ' + this.message);
 
       if(this.subject && this.message){
@@ -80,14 +85,14 @@ module.exports = function(RED) {
     //console.log('nats server connect:' + server);
     var node = this;
 
-    this.on('input', function(msg) {
+    node.on('input', function(msg) {
       this.subject = msg.topic || config.subjec;
+      this.message = msg.payload || config.message
       console.log('subject: ' + this.subject);
 
       if(this.subject){
-        console.log('Hi: '+this.subject);
         this.nc.request(this.subject, function(response) {
-          console.log('Got a response in msg stream: ' + response);
+          //console.log('Got a response in msg stream: ' + response);
           node.send(msg);
         })
         this.nc.publish(this.subject, this.message);
@@ -101,35 +106,4 @@ module.exports = function(RED) {
     });
   }
   RED.nodes.registerType("nats-request",NatsRequestNode);
-
-  function NatsReplyNode(config) {
-    RED.nodes.createNode(this, config);
-
-    this.address = config.address;
-    this.port = config.port;
-    this.user = config.user;
-    this.pass = config.pass;
-
-    var server = 'nats://' + config.user + ':' + config.pass + '@' + this.address + ':' + this.port + '/';
-    this.nc = nats.connect({'servers': [server]});
-    //console.log('nats server connect:' + server);
-    var node = this;
-
-    this.on('input', function(msg) {
-      this.subject = msg.payload.subject || config.subject;
-      this.message = msg.payload.message || config.message;
-      //console.log('subject: ' + this.subject + ' message: ' + this.message);
-
-      if(this.subject && this.message){
-        this.nc.publish(this.subject, this.message);
-      }
-    });
-
-    this.on('close', function() {
-      if (this.nc) {
-        this.nc.close();
-      }
-    });
-  }
-  RED.nodes.registerType("nats-reply",NatsPubNode);
 }
