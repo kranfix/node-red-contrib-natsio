44e89d9bee0cf41121748bd2cbad71d31dee0181
Frank Moreno <frankmoreno1993@gmail.com>
2017-10-22 13:23:07 -0500

    pub node with config node

---
+++ natsio_pub.js
@@ -0,0 +1,27 @@
+var nats = require('nats');
+
+module.exports = function(RED) {
+
+  function NatsPubNode(config) {
+    RED.nodes.createNode(this, config);
+
+    this.server = RED.nodes.getNode(config.server);
+
+    var node = this;
+    node.on('input', function(msg) {
+      var subject = msg.replyTo || msg.topic || config.subject;
+      var message = msg.payload || config.message;
+
+      if(subject && message){
+        this.server.nc.publish(subject, message);
+      }
+    });
+
+    node.on('close', function() {
+      if (node.server.nc) {
+        node.server.nc.close();
+      }
+    });
+  }
+  RED.nodes.registerType("nats-pub",NatsPubNode);
+}
