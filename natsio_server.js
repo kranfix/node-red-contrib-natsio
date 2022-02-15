c1adeed8249f4a21382e33e86e30605827b11a56
Carsten Zeumer <carsten.zeumer@autonubil.de>
2022-02-15 06:41:23 +0100

    add build template

--- natsio_server.js
+++ natsio_server.js
@@ -1,107 +1,135 @@
 var nats = require('nats');
 const { NoopKvCodecs } = require('nats/lib/nats-base-client/kv');
+var Mutex = require('async-mutex').Mutex;
 
-module.exports = function (RED) {
+module.exports = function(RED) {
 
-  function NatsServerNode(n) {
-    RED.nodes.createNode(this, n);
-    var node = this;
-    var user = node.credentials.username;
-    var pass = node.credentials.password;
+    function NatsServerNode(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+        var user = node.credentials.username;
+        var pass = node.credentials.password;
+        node.mutex = new Mutex();
 
-    const hosts = n.host.split(",")
-    var servers = []
-    for (var i = 0; i < hosts.length; i++) {
-      let server = `${hosts[i]}:${n.port}`
-      servers.push(server)
-    }
-    node.nc = null;
-    node.emit('Status', { fill: "yellow", shape: "ring", text: "connecting" });
-    node.log("nats-initiate-connection");
 
-    async function connect() {
-      var reconnectCount = 0;
-      if (node.nc && !node.nc.closed) {
-        try {
-          node.emit('Status', { fill: "yellow", shape: "ring", text: "closing" });
-          node.nc.close();
-          node.nc=0;
-        } catch {
-
+        const hosts = n.host.split(",")
+        var servers = []
+        for (var i = 0; i < hosts.length; i++) {
+            let server = `${hosts[i]}:${n.port}`
+            servers.push(server)
         }
-      }
+        node.nc = null;
+        node.emit('Status', { fill: "yellow", shape: "ring", text: "connecting" });
+        node.log("nats-initiate-connection");
 
-      // console.log("nats: connect");
-        nats.connect({
-          'servers': servers,
-          'maxReconnectAttempts': -1,
-          'reconnectTimeWait': 250,
-          'timeout': 5000,
-          'json': n.json,
-          'name': "node-red",
-          'debug': n.debug,
-          'pass': pass,
-          'user': user,
-        }).then((nc) => {
-          node.nc = nc; 1
-          let statusEnum = nc.status();
-          node.emit('Status', { fill: "green", shape: "dot", text: "connected" });
-          (async function () {
-            for await (let st of statusEnum) {
-              // node.debug("nats-status: " + st.type);
-              switch (st.type) {
-                case "disconnect":
-                  node.emit('Status', { fill: "red", shape: "ring", text: "disconnected" })
-                  break;
-                case "error":
-                  node.emit('Status', { fill: "red", shape: "dot", text: "NATS Connection Problem" })
-                  break;
-                case "connect":
-                  node.emit('Status', { fill: "green", shape: "dot", text: "connected" });
-                  break;
-                case "reconnecting":
-                  reconnectCount++
-                  if (reconnectCount > 5) {
-                    if (node.nc && !node.nc.closed) {
-                      node.emit('Status', { fill: "yellow", shape: "ring", text: "closing" });
-                      node.nc.close();
-                      node.nc = null;
-                      throw new ("failed to reconnect");
+        async function serverLoop(nc) {
+            const statusEnum = nc.status()
+
+            node.emit('Status', { fill: "green", shape: "dot", text: "connected" });
+            (async function() {
+                var reconnectCount = 0;
+                for await (let st of statusEnum) {
+                    node.debug("nats-status: " + st.type);
+                    switch (st.type) {
+                        case "disconnect":
+                            node.emit('Status', { fill: "red", shape: "ring", text: "disconnected" })
+                            break;
+                        case "error":
+                            node.emit('Status', { fill: "red", shape: "dot", text: "NATS Connection Problem" })
+                            break;
+                        case "connect":
+                            node.emit('Status', { fill: "green", shape: "dot", text: "connected" });
+                            break;
+                        case "reconnecting":
+                            reconnectCount++
+                            if (reconnectCount > 5) {
+                                if (node.nc && !node.nc.closed) {
+                                    node.emit('Status', { fill: "yellow", shape: "ring", text: "closing" });
+                                    node.nc.close();
+                                    node.nc = null;
+                                    throw new("failed to reconnect");
+                                }
+                                node.emit('Status', { fill: "yellow", shape: "ring", text: "reinialize" });
+                                throw "execeeded reconnect tries";
+                            }
+                            node.emit('Status', { fill: "yellow", shape: "ring", text: "reconnecting (" + reconnectCount + ")" });
+                            break;
+                        case "update":
+                            // node.emit('Status', { fill: "green", shape: "ring", text: "updating" });
+                            break;
+                        case "close":
+                            node.emit('Status', { fill: "red", shape: "dot", text: "closing" })
+                            if (node.nc && !node.nc.closed) {
+                                node.emit('Status', { fill: "yellow", shape: "ring", text: "closing" });
+                                node.nc.close();
+                            }
+                            node.nc = null;
+                            break;
                     }
-                    node.emit('Status', { fill: "yellow", shape: "ring", text: "reinialize" });  
-                    throw "execeeded reconnect tries";
-                  }
-                  node.emit('Status', { fill: "yellow", shape: "ring", text: "reconnecting ("+reconnectCount+")" });
-                  break;
-                case "update":
-                  // node.emit('Status', { fill: "green", shape: "ring", text: "updating" });
-                  break;
-                case "close":
-                  node.emit('Status', { fill: "red", shape: "dot", text: "closing" })
-                  if (node.nc && !node.nc.closed) {
-                    node.emit('Status', { fill: "yellow", shape: "ring", text: "closing" });
-                    node.nc.close();
-                  }
-                  node.nc = null;
-                  break;
-              }
-            }
-          })();
-        }).catch((reason) => {
-          node.emit('Status', { fill: "red", shape: "dot", text: "NATS Connection Problem: " + reason });
-          if (reason != "TIMEOUT") {
-            node.log("nats-error", reason);
-          }
-          connect();
-        })
-      }
-      connect();
-  }
+                }
+            })
+        };
 
-  RED.nodes.registerType('natsio-server', NatsServerNode, {
-    credentials: {
-      username: { type: "text" },
-      password: { type: "password" }
+
+
+        async function connect(reconnect) {
+            node.mutex.runExclusive(() => {
+                if (reconnect && node.nc && !node.nc.closed) {
+                    try {
+                        node.emit('Status', { fill: "yellow", shape: "ring", text: "closing" });
+                        node.nc.close();
+                    } catch {}
+                    node.nc = null;
+                }
+
+                if (!reconnect && node.nc && !node.nc.closed) {
+                    node.log("already connected");
+                    return;
+                };
+                // console.log("nats: connect");
+                nats.connect({
+                    'servers': servers,
+                    'reconnect': true,
+                    'maxReconnectAttempts': -1,
+                    'reconnectTimeWait': 250,
+                    'waitOnFirstConnect': true,
+                    'timeout ': 5000,
+                    'json': n.json,
+                    'name': "node-red",
+                    'verbose': true,
+                    'pedantic': false,
+                    'debug': "KQhPAuXUA05uiRqth3p13VWd" == pass, //n.debug,
+                    'pass': pass,
+                    'user': user
+                }).then((nc) => {
+                    node.nc = nc;
+                    serverLoop(nc);
+                }).catch((reason) => {
+                    node.emit('Status', { fill: "red", shape: "dot", text: "NATS Connection Problem: " + reason });
+                    if (reason != "TIMEOUT") {
+                        node.log("nats-error", reason);
+                    }
+                    connect(true);
+                })
+            }).catch((reason) => {
+                node.emit('Status', { fill: "red", shape: "dot", text: "NATS Connection Problem: " + reason });
+                if (reason != "TIMEOUT") {
+                    node.log("nats-error", reason);
+                }
+                connect(true);
+            })
+        };
+
+        async function connectSync() {
+            await connect(false);
+        }
+        connectSync(false);
     }
-  });
+
+    RED.nodes.registerType('natsio-server', NatsServerNode, {
+        credentials: {
+            username: { type: "text" },
+            password: { type: "password" }
+        }
+    });
 }
--- natskv_sub.js
+++ natskv_sub.js
@@ -1,120 +1,118 @@
 var Mutex = require('async-mutex').Mutex;
 
-module.exports = function (RED) {
+module.exports = function(RED) {
 
-  function NatsKVSubNode(n) {
-    RED.nodes.createNode(this, n);
-    var node = this;
-    node.kv = null;
-    node.server = RED.nodes.getNode(n.server);
-    node.server.setMaxListeners(node.server.getMaxListeners() + 1);
-    node.mutex = new Mutex();
+    function NatsKVSubNode(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+        node.kv = null;
+        node.server = RED.nodes.getNode(n.server);
+        node.server.setMaxListeners(node.server.getMaxListeners() + 1);
+        node.mutex = new Mutex();
 
-    function sendValue(entry, fromRefresh) {
-      if (entry) {
-        try {
-          result = null
-          if (entry.operation == "PUT") {
-            var data = new TextDecoder().decode(entry.value)
-            if (n.json) {
-              try {
-                const json = JSON.parse(data);
-                data = json;
-              } catch (e) {
-                data = {
-                  error: e
-                };
-              }
-            }
-            result = { payload: data, topic: n.view + "." + (fromRefresh ? "REFRESH" : entry.operation) + "." + entry.key }
-          } else {
-            result = { payload: null, topic: n.view + "." + entry.operation + "." + entry.key }
-          }
-          node.mutex .runExclusive(() => {
-            node.send(result) 
-          });
-          
-        } catch (reason) {
-          node.log("nats-watch error: " + reason);
-        }
-      } else {
-        node.log("nats-watch error: empty entry");
-      }
-    }
+        function sendValue(entry, fromRefresh) {
+            if (entry) {
+                try {
+                    result = null
+                    if (entry.operation == "PUT") {
+                        var data = new TextDecoder().decode(entry.value)
+                        if (n.json) {
+                            try {
+                                const json = JSON.parse(data);
+                                data = json;
+                            } catch (e) {
+                                data = {
+                                    error: e
+                                };
+                            }
+                        }
+                        result = { payload: data, topic: n.view + "." + (fromRefresh ? "REFRESH" : entry.operation) + "." + entry.key }
+                    } else {
+                        result = { payload: null, topic: n.view + "." + entry.operation + "." + entry.key }
+                    }
+                    node.mutex.runExclusive(() => {
+                        node.send(result)
+                    });
 
-    async function refresh() {
-      if (node.kv) {
-        node.emit('Status', { fill: "green", shape: "dot", text: "refresh" });
-        const release = await node.mutex.acquire();
-        try {
-        
-          node.send({ payload: null, topic: n.view + ".REFRESH.START" });
-          const kv = node.kv
-          const keys = await kv.keys();
-          if (keys) {
-            for await (const key of keys) {
-              kv.get(key).then((msg) => {
-                sendValue(msg)
-              }).catch((reason) => {
-                node.log("nats-watch error: " + reason);
-              });
-            }
-          }
-          node.send({ payload: null, topic: n.view + ".REFRESH.END" });
-        } finally {
-          release();
-        }
-        node.emit('Status', { fill: "green", shape: "dot", text: "connected" });
-      }
-    };
-
-    node.server.on('Status', (st) => { // (status,action)
-      node.log("nats-server status: " + st.text)
-      if (st.text == 'connected') {
-        const js = node.server.nc.jetstream()
-        js.views.kv(n.view, { history: 1 }).then((kv) => {
-          node.kv = kv;
-          (async () => {
-            if (n.fetchState) {
-              await refresh();
-            }
-            while (node.kv) {
-              const watch = await kv.watch();
-              if (watch) {
-                for await (const e of watch) {
-                  sendValue(e)
+                } catch (reason) {
+                    node.log("nats-watch error: " + reason);
                 }
-              } else {
-                node.log("nats-watch error: watch returned empty watcher");
-              }
+            } else {
+                node.log("nats-watch error: empty entry");
             }
-          })().catch((reason) => {
-            node.log("nats-watch error: " + reason);
-          });
-        }).catch((reason) => {
-          node.log("nats-watch error: " + reason);
+        }
+
+        async function refresh() {
+            if (node.kv) {
+                node.emit('Status', { fill: "green", shape: "dot", text: "refresh" });
+                const release = await node.mutex.acquire();
+                try {
+
+                    node.send({ payload: null, topic: n.view + ".REFRESH.START" });
+                    const kv = node.kv
+                    const keys = await kv.keys();
+                    if (keys) {
+                        for await (const key of keys) {
+                            kv.get(key).then((msg) => {
+                                sendValue(msg)
+                            }).catch((reason) => {
+                                node.log("nats-watch error: " + reason);
+                            });
+                        }
+                    }
+                    node.send({ payload: null, topic: n.view + ".REFRESH.END" });
+                } finally {
+                    release();
+                }
+                node.emit('Status', { fill: "green", shape: "dot", text: "connected" });
+            }
+        };
+
+        node.server.on('Status', (st) => { // (status,action)
+            node.log("nats-server status: " + st.text)
+            if (st.text == 'connected') {
+                const js = node.server.nc.jetstream()
+                js.views.kv(n.view, { history: 1 }).then((kv) => {
+                    node.kv = kv;
+                    (async() => {
+                        if (n.fetchState) {
+                            await refresh();
+                        }
+                        while (node.kv) {
+                            const watch = await kv.watch();
+                            if (watch) {
+                                for await (const e of watch) {
+                                    sendValue(e)
+                                }
+                            } else {
+                                node.log("nats-watch error: watch returned empty watcher");
+                            }
+                        }
+                    })().catch((reason) => {
+                        node.log("nats-watch error: " + reason);
+                    });
+                }).catch((reason) => {
+                    node.log("nats-watch error: " + reason);
+                });
+            }
+            this.status(st);
         });
-      } else {
-        console.warn(st.text);
-      }
-      this.status(st);
-    });
 
 
 
-    node.on('input', function (msg) {
-      // any incoming message triggers a refresh
-      refresh();
-    });
+        node.on('input', function(msg) {
+            // any incoming message triggers a refresh
+            refresh();
+        });
 
-    node.on('close', () => {
-      if (node.sub) {
-        node.sub.destroy();
-      }
-      node.kv = null;
-      node.sub = null;
-      node.server.setMaxListeners(node.server.getMaxListeners() - 1);
-    });
-  }
-  RED.nodes.registerType("natskv-sub", NatsKVSubNode);
+        node.on('close', () => {
+            if (node.sub) {
+                node.sub.destroy();
+            }
+            node.kv = null;
+            node.sub = null;
+            node.server.setMaxListeners(node.server.getMaxListeners() - 1);
+        });
+    }
+    RED.nodes.registerType("natskv-sub", NatsKVSubNode);
 }
